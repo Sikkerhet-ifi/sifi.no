ac2beae44228ce8fc62c8135c11d059fcfbf16b5
vfaergestad <49147564+vfaergestad@users.noreply.github.com>
2024-10-23 11:49:01 +0200

    Back to basics

--- src/app/arrangementer/[slug]/page.tsx
+++ src/app/arrangementer/[slug]/page.tsx
@@ -6,64 +6,46 @@ import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
 
-// Set revalidation time for ISR
-export const revalidate = 30;
-
-// Get all possible events for static compiling
-export async function generateStaticParams() {
-  // Sanity query to get all slugs for your documents
-  const query = `*[_type == "event"]{ "slug": slug.current }`;
-
-  // Fetch the slugs from Sanity
-  const slugs = await client.fetch(query);
-
-  // Return the list of params as required by Next.js
-  return slugs.map((event: { slug: string }) => ({
-    slug: event.slug, // The slug used in the route
-  }));
-}
-
-const POST_QUERY = `*[_type == "event" && slug.current == $slug][0]`;
+const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
 
 const { projectId, dataset } = client.config();
 const urlFor = (source: SanityImageSource) =>
   projectId && dataset
     ? imageUrlBuilder({ projectId, dataset }).image(source)
     : null;
-
+const options = { next: { revalidate: 30 } };
 export default async function PostPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  // Remove options to ensure static generation
-  const event = await client.fetch<SanityDocument>(POST_QUERY, params);
-  if (event == null) {
+  const post = await client.fetch<SanityDocument>(POST_QUERY, params, options);
+  console.log(post);
+  if (post == null) {
     return notFound();
   }
-  const eventImageUrl = event.image
-    ? urlFor(event.image)?.width(550).height(310).url()
+  const postImageUrl = post.image
+    ? urlFor(post.image)?.width(550).height(310).url()
     : null;
-
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
       <Link href="/" className="hover:underline">
         ← Tilbake til forsiden
       </Link>
-      {eventImageUrl && (
+      {postImageUrl && (
         <Image
-          src={eventImageUrl}
-          alt={event.title}
+          src={postImageUrl}
+          alt={post.title}
           className="aspect-video rounded-xl"
           width="550"
           height="310"
         />
       )}
-      <h1 className="text-4xl font-bold mb-8">{event.title}</h1>
+      <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
       <div className="prose">
-        <p>Published: {new Date(event.publishedAt).toLocaleDateString()}</p>
-        <p>Tidspunkt: {new Date(event.eventStart).toLocaleDateString()} </p>
-        {Array.isArray(event.body) && <PortableText value={event.body} />}
+        <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
+        <p>Tidspunkt: {new Date(post.eventStart).toLocaleDateString()} </p>
+        {Array.isArray(post.body) && <PortableText value={post.body} />}
       </div>
     </main>
   );
